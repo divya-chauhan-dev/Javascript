c7b63cc50f5e0080836c01c46872158510de857a
divya-chauhan-dev <divya198877@gmail.com>
2024-10-21 19:31:20 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,40 @@
+"use strict";   // treat all js code as new version
+
+// alert(3+3) // will throw an error as we are using node js not any browser
+
+// we must take care of code redeability
+// for example
+/*console.log("Hii!");  console.log("How are you?")*/
+
+// does not look good at all
+
+//instead we can write as follows
+
+console.log("Hii!")
+console.log("How are you?")
+
+
+
+/*primitive data types
+number => upto 2 to the power 50
+bigint =>
+string =""
+boolean = True/False
+null = standalone value
+undefined =>
+symbol => for uniqueness
+*/
+
+let name = "Divya"
+let age = 22
+//let isLoggedIn = True/false
+
+
+let status; // this will shows undefined status
+
+
+// object is another data type
+
+console.log(typeof "Divya");   // type is string
+console.log(typeof null);      // type is object
+console.log(typeof undefined); //type is undefined
