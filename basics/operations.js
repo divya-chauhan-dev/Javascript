bedf418a0b95e5b41c44ea61823581477d854526
divya-chauhan-dev <divya198877@gmail.com>
2024-10-22 19:35:10 +0000

    operations

---
+++ basics/operations.js
@@ -0,0 +1,59 @@
+// Here we will learn about different operators
+
+//let value = 4
+//let negvalue = -value;
+//console.log(negvalue);
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);             //these are basic operations
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+*/
+
+/*
+let str1 = "Self - love"
+let str2 = " is must.";
+let str3 = str1 + str2;       //like this we can add two strings as well and also apply many other operations on strings
+console.log(str3);
+*/
+
+// console.log("1"+ 2);
+// console.log(1 +"2");
+//console.log("1"+ 2 + 2);       /*operators works accordingly as what comes first like if at first it is srting then the*/
+//console.log(1 + 2 + "2");      /* whole number will be convert to string */
+
+// like first output will be 12
+// second will be 12 as well
+// third will be 122
+// and fourth will be 32
+
+/* but this is a bad practice as you are suggested to put some brackets on whatever you wanted to compute first and for good 
+code readability as well LIKE BELOW */
+
+// console.log((2+2)*(10%3));
+// console.log("");
+
+
+// some tricky operations that we should not use because they used to create confusion
+
+console.log(+true);         // gives 1
+// console.log(true +);    // gives error
+console.log(+"");          // gives 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
