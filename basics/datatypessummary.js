b938023982c20157110c5b95b536835e5a9def75
divya-chauhan-dev <divya198877@gmail.com>
2024-10-24 05:52:08 +0000

    changes made in file

--- basics/datatypessummary.js
+++ basics/datatypessummary.js
@@ -51,11 +51,76 @@ let myFunction = function(){
 //Note : if you want to master javascript then master its objects and browser event
 
 
-console.log(typeof Name);                // output is object
-console.log(typeof myObject);            // again object
-console.log(typeof myFunction);          // function
+// console.log(typeof Name);                // output is object
+// console.log(typeof myObject);            // again object
+// console.log(typeof myFunction);          // function
 
 //also js is dynamically typed language
+
+
+
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+/* 
+there are types of memory :
+1. Stack (used for all the primitive data types i.e. these all are call
+          by value or here we got a copy of the original value or the 
+          variable that we have declared so that all the changes that
+          we made just only happens to that copied value not to the orginal one)
+          
+          
+2. Heap (used for Non-Primitive data types i.e. these all are call by reference
+         or we can say that here only address of memory location or reference of 
+         the original value)
+
+         */
+
+
+
+
+
+//Example of Stack and Heap :
+let myScore = 89;
+let anotherScore = myScore;
+// console.log(anotherScore);
+anotherScore = 99;
+console.log(myScore);
+console.log(anotherScore);
+
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne;
+console.log(userTwo);
+
+userTwo.email = "divya@googledotcom"
+console.log(userOne.email)
+
+
+
+//hence this is all about stack and heap
+
+
+
+
+      
+
+
+
+
+
+
+
+
 
 
 
